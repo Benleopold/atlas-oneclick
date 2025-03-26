4da9e4a1238663b22f5a35d2b5bf9835fedcd37a
Benleopold <beherenowben@gmail.com>
2025-03-25 20:45:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def read_root():
     return {
         "message": "Atlas is live!",
         "huggingface": os.getenv("HF_API_KEY"),
-        "openai": os.getenv("OPENAI_API_KEY"),
-        "anthropic": os.getenv("ANTHROPIC_API_KEY"),
+        "openai": os.getenv("OPENAI_API_KEY_ATLAS"),
+        "anthropic": os.getenv("ANTHROPIC_API_KEY_ATLAS"),
         "db": os.getenv("DATABASE_URL")
     }
     
